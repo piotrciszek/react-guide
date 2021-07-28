1e4af25515c3e59e6de96bd19ab739cda5c5cfd2
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2021-07-28 23:29:42 +0200

    new component expenses

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,25 +1,21 @@
-import './ExpenseItem.css'; //style componets
+import ExpenseDate from './ExpenseDate';
+import "./ExpenseItem.css"; //style componets
+
 
 function ExpenseItem(props) {
-    // const expenseDate = new Date(2021, 28, 2);
-    // const expenseTitle = 'Car Insurance';
-    // const expenseAmount = 294.67; 
-    const month = props.date.toLocaleString('en-US', {month: 'long'});
-    const year = props.date.getFullYear();
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-    return (
-      <div className="expense-item">
-        <div>
-            <div>{month}</div>
-            <div>{year}</div>
-            <div>{day}</div>
-        </div>
-        <div className="expense-item__description">
-          <h2>{props.title}</h2>
-          <div className="expense-item__price">{props.amount}</div>
-        </div>
+  // const expenseDate = new Date(2021, 28, 2);
+  // const expenseTitle = 'Car Insurance';
+  // const expenseAmount = 294.67;
+
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default ExpenseItem;
